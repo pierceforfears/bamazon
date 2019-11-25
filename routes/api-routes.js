d75a01b8ce9254c7586ea555128d7553d0628837
Robert Pierce <pierce1673@gmail.com>
2019-11-25 17:20:34 -0500

    re-installing packages

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -12,37 +12,50 @@ var db = require("../models");
 // =============================================================
 module.exports = function (app) {
 
-    // GET route for getting all of the todos
+    // GET route for getting all of the products
     app.get("/api/product", function (req, res) {
         // Write code here to retrieve all of the todos from the database and res.json them
         // back to the user
-        db.Product.findAll({}).then(function (results) {
-            res.json(results);
+        db.Product.findAll({}).then(function (dbPost) {
+            res.json(dbPost);
         });
 
     });
 
-    // POST route for saving a new todo. We can create todo with the data in req.body
+    // POST route for saving a new todo. We can create product with the data in req.body
     app.post("/api/product", function (req, res) {
         // Write code here to create a new todo and save it to the database
         // and then res.json back the new todo to the user
-        db.Todo.create({ productName: req.body.productName, departmentName: req.body.departmentName, price: req.body.price, stockQuantity: req.body.stockQuantity }).then(function (results) {
-            res.end(results);
+        db.Todo.create({ productName: req.body.productName, departmentName: req.body.departmentName, price: req.body.price, stockQuantity: req.body.stockQuantity }).then(function (dbPost) {
+            res.end(dbPost);
         });
 
     });
 
-    // DELETE route for deleting todos. We can get the id of the todo to be deleted from
+    // DELETE route for deleting products. We can get the id of the todo to be deleted from
     // req.params.id
     app.delete("/api/product/:id", function (req, res) {
-        db.Todo.destroy({ where: req.params.id }).then(function () {
-            res.end();
-        });
+        db.Product.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+            .then(function (dbPost) {
+                res.json(dbPost);
+            });
 
     });
 
-    // PUT route for updating todos. We can get the updated todo data from req.body
+    // PUT route for updating todos. We can get the updated product data from req.body
     app.put("/api/product", function (req, res) {
-
+        db.Product.update(req.body,
+            {
+                where: {
+                    id: req.body.id
+                }
+            })
+            .then(function (dbPost) {
+                res.json(dbPost);
+            });
     });
 };
