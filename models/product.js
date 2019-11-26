b73472ffa21908dfc35dbb283a155d239e7db870
Robert Pierce <pierce1673@gmail.com>
2019-11-26 18:53:15 -0500

    migration of database using sequelize

--- models/product.js
+++ models/product.js
@@ -1,29 +1,21 @@
-"use strict";
 module.exports = function(connection, Sequelize) {
-  const Product = connection.define(
-    "Product",
-    {
-      productName: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      departmentName: {
-        type: Sequelize.STRING,
-        allowNull: false
-      },
-      price: {
-        type: Sequelize.DECIMAL(10, 2),
-        allowNull: false
-      },
-      stockQuantity: {
-        type: Sequelize.INTEGER,
-        allowNull: false
-      }
+  const Product = connection.define("Product", {
+    productName: {
+      type: Sequelize.STRING,
+      allowNull: false
     },
-    {}
-  );
-  Product.associate = function(models) {
-    // associations can be defined here
-  };
+    departmentName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    price: {
+      type: Sequelize.DECIMAL(10, 2),
+      allowNull: false
+    },
+    stockQuantity: {
+      type: Sequelize.INTEGER,
+      allowNull: false
+    }
+  });
   return Product;
 };
