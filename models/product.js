00cc1cd1452e63e08ad68107d3f8e334e6211f9f
Robert Pierce <pierce1673@gmail.com>
2019-11-26 16:28:07 -0500

    product model update

--- models/product.js
+++ models/product.js
@@ -1,13 +1,17 @@
-'use strict';
-module.exports = function (sequelize, DataTypes) {
-    const Product = sequelize.define('Product', {
-        productName: DataTypes.STRING,
-        departmentName: DataTypes.INTEGER,
-        price: DataTypes.DECIMAL,
-        stockQuantity: DataTypes.STRING
-    }, {});
-    Product.associate = function (models) {
-        // associations can be defined here
-    };
-    return Product;
+"use strict";
+module.exports = function(sequelize, DataTypes) {
+  const Product = sequelize.define(
+    "Product",
+    {
+      productName: DataTypes.STRING,
+      departmentName: DataTypes.INTEGER,
+      price: DataTypes.DECIMAL,
+      stockQuantity: DataTypes.STRING
+    },
+    {}
+  );
+  Product.associate = function(models) {
+    // associations can be defined here
+  };
+  return Product;
 };
