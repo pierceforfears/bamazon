dc45b9057cac86b226a2591fca032f8190359859
Robert Pierce <pierce1673@gmail.com>
2019-11-26 18:20:59 -0500

    migration of database using sequelize

--- models/product.js
+++ models/product.js
@@ -12,7 +12,7 @@ module.exports = function(sequelize, DataTypes) {
         allowNull: false
       },
       price: {
-        type: DataTypes.DECIMAL,
+        type: DataTypes.DECIMAL(10, 2),
         allowNull: false
       },
       stockQuantity: {
--- seeders/db-seed.js
+++ seeders/db-seed.js
@@ -1,76 +1,70 @@
-"use strict";
+const db = require("../models");
 
-module.exports = {
-  up: function(queryInterface, Sequelize) {
-    return queryInterface.bulkInsert(
-      "Product",
-      [
-        {
-          productName: "MODS 40 Foot Tiny Home",
-          departmentName: "Home and Garden",
-          price: 36000.0,
-          stockQuantity: 100
-        },
-        {
-          productName: "1500 Live Ladybugs",
-          departmentName: "Pets",
-          price: 17.0,
-          stockQuantity: 100
-        },
-        {
-          productName: "Uranium Ore",
-          departmentName: "Sporting Goods",
-          price: 40.0,
-          stockQuantity: 100
-        },
-        {
-          productName: "Organic Mushroom Farm Grow Kit",
-          departmentName: "Home and Garden",
-          price: 17.0,
-          stockQuantity: 100
-        },
-        {
-          productName: "Cat Butt Tissue Holder",
-          departmentName: "Home and Garden",
-          price: 36.0,
-          stockQuantity: 100
-        },
-        {
-          productName: "Fake Human Finger Bones",
-          departmentName: "Oddities and Ends",
-          price: 15.0,
-          stockQuantity: 100
-        },
-        {
-          productName: "PB&J of the Month Club",
-          departmentName: "Prepared Foods",
-          price: 269.0,
-          stockQuantity: 100
-        },
-        {
-          productName: "Bacon Scented Mustache",
-          departmentName: "Personal Hygiene",
-          price: 4.0,
-          stockQuantity: 100
-        },
-        {
-          productName: "Godzilla Lawn Gnome",
-          departmentName: "Home and Garden",
-          price: 22.0,
-          stockQuantity: 100
-        },
-        {
-          productName: "Dr. Pickle Lip Balm",
-          departmentName: "Personal Hygiene",
-          price: 12.0,
-          stockQuantity: 100
-        }
-      ],
-      {}
-    );
+const items = [
+  {
+    productName: "MODS 40 Foot Tiny Home",
+    departmentName: "Home and Garden",
+    price: 36000.0,
+    stockQuantity: 100
   },
-
-  down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable("Product");
+  {
+    productName: "1500 Live Ladybugs",
+    departmentName: "Pets",
+    price: 17.0,
+    stockQuantity: 100
+  },
+  {
+    productName: "Uranium Ore",
+    departmentName: "Sporting Goods",
+    price: 40.0,
+    stockQuantity: 100
+  },
+  {
+    productName: "Organic Mushroom Farm Grow Kit",
+    departmentName: "Home and Garden",
+    price: 17.0,
+    stockQuantity: 100
+  },
+  {
+    productName: "Cat Butt Tissue Holder",
+    departmentName: "Home and Garden",
+    price: 36.0,
+    stockQuantity: 100
+  },
+  {
+    productName: "Fake Human Finger Bones",
+    departmentName: "Home and Garden",
+    price: 15.0,
+    stockQuantity: 100
+  },
+  {
+    productName: "PB & Jelly of the Month Club",
+    departmentName: "Home and Garden",
+    price: 269.0,
+    stockQuantity: 100
+  },
+  {
+    productName: "Bacon Scented Mustache",
+    departmentName: "Home and Garden",
+    price: 4.0,
+    stockQuantity: 100
+  },
+  {
+    productName: "Godzilla Lawn Gnome",
+    departmentName: "Home and Garden",
+    price: 22.0,
+    stockQuantity: 100
+  },
+  {
+    productName: "Dr. Pickle Lip Balm",
+    departmentName: "Home and Garden",
+    price: 12.0,
+    stockQuantity: 100
   }
-};
+];
+
+db.sequelize.sync({ force: true }).then(function() {
+  db.Product.bulkCreate(items).then(function(rows) {
+    db.sequelize.close();
+  });
+});
