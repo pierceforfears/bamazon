e1fe65c09fd164a7cca02a24fc0a82dbff369755
Robert Pierce <pierce1673@gmail.com>
2019-11-26 21:05:40 -0500

    showing cart upon adding an item to cart

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,8 @@
 let cart = [];
 //this is the function to render all items in a database.
-const renderItems = function (items) {
-    items.forEach(function (item) {
-        let newProduct = $(` <tr>
+const renderItems = function(items) {
+  items.forEach(function(item) {
+    let newProduct = $(` <tr>
           <td class="id">${item.id}</td>
           <td class="productName">${item.productName}</td>
           <td class="departmentName">${item.departmentName}</td>
@@ -11,77 +11,72 @@ const renderItems = function (items) {
           <td class="quantity"><input class="buy1" id="input"></td>
           <td><button type="button" class="btn btn-info">Add to Cart</button><td>
         </tr>`);
-        $(".tbodypage").append(newProduct);
-    });
+    $(".tbodypage").append(newProduct);
+  });
 };
 
-const clearInput = function () {
-    $("[id =input]").val("");
+const clearInput = function() {
+  $("[id =input]").val("");
 };
 
-const validate = function (item) {
-    if (item.incart > item.instock) {
-        $(".alert").removeClass("hide");
-        clearInput();
-    } else if (isNaN(item.incart)) {
-        $(".alert").removeClass("hide");
-        clearInput();
-    }
-    else {
-        cart.push(item);
-        clearInput();
-    }
+const validate = function(item) {
+  if (item.incart > item.instock) {
+    $(".alert").removeClass("hide");
+    clearInput();
+  } else if (isNaN(item.incart)) {
+    $(".alert").removeClass("hide");
+    clearInput();
+  } else {
+    cart.push(item);
+    clearInput();
+  }
 };
-
-
 
 //this is the document ready function that says to render all the items in the database, to the table.
 $(document).ready(() => {
-    $.ajax({
-        url: "/api/products",
-        type: "GET"
-    }).then(function (rows) {
-        renderItems(rows);
-    });
-    //this says when the add to cart button is clicked, to then add the data to the array.
-    $(".table-body").on("click", ".btn", function () {
-        $(".alert").addClass("hide");
-        const item = {
-            id: $(this)
-                .parents("tr")
-                .find(".id")
-                .text(),
-            name: $(this)
-                .parents("tr")
-                .find(".productName")
-                .text(),
-            department: $(this)
-                .parents("tr")
-                .find(".departmentName")
-                .text(),
-            price: $(this)
-                .parents("tr")
-                .find(".price")
-                .text(),
-            instock: $(this)
-                .parents("tr")
-                .find(".stockQuantity")
-                .text(),
-            incart: $(this)
-                .parents("tr")
-                .find(".buy1")
-                .val()
-        };
+  $.ajax({
+    url: "/api/products",
+    type: "GET"
+  }).then(function(rows) {
+    renderItems(rows);
+  });
+  //this says when the add to cart button is clicked, to then add the data to the array.
+  $(".table-body").on("click", ".btn", function() {
+    $(".alert").addClass("hide");
+    const item = {
+      id: $(this)
+        .parents("tr")
+        .find(".id")
+        .text(),
+      name: $(this)
+        .parents("tr")
+        .find(".productName")
+        .text(),
+      department: $(this)
+        .parents("tr")
+        .find(".departmentName")
+        .text(),
+      price: $(this)
+        .parents("tr")
+        .find(".price")
+        .text(),
+      instock: $(this)
+        .parents("tr")
+        .find(".stockQuantity")
+        .text(),
+      incart: $(this)
+        .parents("tr")
+        .find(".buy1")
+        .val()
+    };
 
-        validate(item);
+    validate(item);
 
-        console.table(cart);
-    });
+    console.table(cart);
 
-    $(".btncart").on("click", function () {
-        $(".modal-body").empty();
+    $(".modal-body").empty();
 
-        $('.modal-body').append(`  <table class="table">
+    $(".modal-body").append(`  <table class="table">
     <thead class="thead-dark">
       <tr>
         <th scope="col">Id</th>
@@ -94,49 +89,81 @@ $(document).ready(() => {
     <tbody class="tbodymodal table-body"></tbody>
   </table>
   <div class="totalCart"></div>`);
-        let totalcost = 0;
-        for (let i = 0; i < cart.length; i++) {
-            totalcost += (parseFloat(cart[i].price) * parseFloat(cart[i].incart.padStart(3, 0)))
-            console.log(cart[i]);
-            $(".tbodymodal").append(`<tr>
+    let totalcost = 0;
+    for (let i = 0; i < cart.length; i++) {
+      totalcost +=
+        parseFloat(cart[i].price) * parseFloat(cart[i].incart.padStart(3, 0));
+      console.log(cart[i]);
+      $(".tbodymodal").append(`<tr>
             <td class="cartid">${cart[i].id}</td>
             <td class="cart-productName">${cart[i].name}</td>
             <td class="cart-departmentName">${cart[i].department}</td>
             <td class="cartPrice">$${cart[i].price}</td>
             <td class="cartQuantity">${cart[i].incart}</td>
             </tr>`);
+    }
+    $(".totalCart").empty();
+    $(".totalCart").append(`<h4>TOTAL PRICE:   $${totalcost}</h4>`);
+    $(".modal").modal("show");
+  });
 
-        }
-        $(".totalCart").empty();
-        $(".totalCart").append(`<h4>TOTAL PRICE:   $${totalcost}</h4>`)
-        $(".modal").modal("show");
-    });
+  $(".btncart").on("click", function() {
+    $(".modal-body").empty();
 
-
+    $(".modal-body").append(`  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">Id</th>
+        <th scope="col">Product</th>
+        <th scope="col">Department</th>
+        <th scope="col">Price</th>
+        <th scope="col">Quanity</th>
+      </tr>
+    </thead>
+    <tbody class="tbodymodal table-body"></tbody>
+  </table>
+  <div class="totalCart"></div>`);
+    let totalcost = 0;
+    for (let i = 0; i < cart.length; i++) {
+      totalcost +=
+        parseFloat(cart[i].price) * parseFloat(cart[i].incart.padStart(3, 0));
+      console.log(cart[i]);
+      $(".tbodymodal").append(`<tr>
+            <td class="cartid">${cart[i].id}</td>
+            <td class="cart-productName">${cart[i].name}</td>
+            <td class="cart-departmentName">${cart[i].department}</td>
+            <td class="cartPrice">$${cart[i].price}</td>
+            <td class="cartQuantity">${cart[i].incart}</td>
+            </tr>`);
+    }
+    $(".totalCart").empty();
+    $(".totalCart").append(`<h4>TOTAL PRICE:   $${totalcost}</h4>`);
+    $(".modal").modal("show");
+  });
 });
 
-$(".btnPurchase").on("click", function () {
-
-    for (let i = 0; i < cart.length; i++) {
-        const inCartnow = cart[i].incart;
-        const inStocknow = cart[i].instock;
-        const newStock = function (a, b) {
-            return a - b;
-        }
-        stockUpdate = newStock(inStocknow, inCartnow)
-        $.ajax({
-            url: `/api/products/${cart[i].id}`,
-            type: "PUT",
-            data: `stockQuantity= ${stockUpdate}`
-        }).then(function (data) {
-            $('.tbodypage').empty();
-            renderItems(data);
-        }).catch(function (data) {
-            console.log(data);
-        })
-        $('.modal-body').empty();
-        $('.modal-body').append("Weird stuff coming your way!")
-
-    }
-    cart = [];
+$(".btnPurchase").on("click", function() {
+  for (let i = 0; i < cart.length; i++) {
+    const inCartnow = cart[i].incart;
+    const inStocknow = cart[i].instock;
+    const newStock = function(a, b) {
+      return a - b;
+    };
+    stockUpdate = newStock(inStocknow, inCartnow);
+    $.ajax({
+      url: `/api/products/${cart[i].id}`,
+      type: "PUT",
+      data: `stockQuantity= ${stockUpdate}`
+    })
+      .then(function(data) {
+        $(".tbodypage").empty();
+        renderItems(data);
+      })
+      .catch(function(data) {
+        console.log(data);
+      });
+    $(".modal-body").empty();
+    $(".modal-body").append("Weird stuff coming your way!");
+  }
+  cart = [];
 });
